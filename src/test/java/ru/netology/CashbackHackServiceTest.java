460694019ff92df414ff37b4f6eb57e533e38d46
Azez <aleksanderterar@gmail.com>
2023-07-25 23:55:30 +0300

    TestNG

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,12 +1,52 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    public void shouldLimitValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        AssertJUnit.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldUnderLimitValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        AssertJUnit.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldBoundaryValue() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        AssertJUnit.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldZeroValue() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        AssertJUnit.assertEquals(actual, expected);
     }
 }
